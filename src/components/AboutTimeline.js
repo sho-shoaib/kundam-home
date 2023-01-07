143f9a9a7319e13cac8c6172ce51093131141a76
Shoaib Shaikh <shaikhsho292@gmail.com>
2023-01-07 18:17:16 +0530

    changes

--- src/components/AboutTimeline.js
+++ src/components/AboutTimeline.js
@@ -1,30 +1,45 @@
 import React from "react";
 import useWindowDimensions from "./getWindowDimensions";
+import circle from "../images/circle.png";
+import goldenLine from "../images/golden-line.png";
 
 const data = [
   {
     position: "right",
-    date: "1960's",
+    date: "1978",
     heading: "Lorem",
-    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore aut consequuntur officiis.",
+    body: "Established in 1978, Kundan is closely held company with strong legacy of 4 decades.",
   },
   {
     position: "left",
-    date: "1960's",
+    date: "1985",
     heading: "Lorem",
-    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore aut consequuntur officiis.",
+    body: "Engaged in manufacturing of wide range of fasteners, brass components, turned components, sheet metal components in stainless steel and various specialized grades.",
+    big: true,
   },
   {
     position: "right",
-    date: "1960's",
+    date: "1990",
     heading: "Lorem",
-    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore aut consequuntur officiis.",
+    body: "Over 150,000 in house product design capability.",
   },
   {
     position: "left",
-    date: "1960's",
+    date: "2000",
     heading: "Lorem",
-    body: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolore aut consequuntur officiis.",
+    body: "Total manufacturing capacity of 7,500 MT p.a. with additional land available for expansion.",
+  },
+  {
+    position: "right",
+    date: "2010",
+    heading: "Lorem",
+    body: "Distribution & Marketing offices across major cities in India along with USA & China.",
+  },
+  {
+    position: "left",
+    date: "2019",
+    heading: "Lorem",
+    body: "Well established presence in International markets with exports to 50+ countries.",
   },
 ];
 
@@ -43,16 +58,24 @@ const AboutTimeline = () => {
               className='mx-auto grid grid-cols-12 gap-8 justify-center'
             >
               <div className='sm:col-span-6 col-span-4 h-full flex flex-col items-center gap-4 p-4'>
-                <div
-                  className='h-24 w-10 rounded-full '
-                  style={{ backgroundColor: "#FCBF02" }}
-                ></div>
+                <div className='h-10 w-10 rounded-full'>
+                  <img
+                    src={circle}
+                    className='w-full h-full object-cover'
+                    alt=''
+                  />
+                </div>
                 <span className='sm:text-4xl text-2xl'>{date}</span>
                 <div
-                  className='border-dashed border-r-4 w-1 h-full '
+                  className='w-10 h-full overflow-hidden'
                   style={{ borderColor: "#FCBF02" }}
                 >
-                  {" "}
+                  <img
+                    src={goldenLine}
+                    alt='line'
+                    className='w-full h-full object-fill'
+                    style={{ transform: "scale(6)" }}
+                  />
                 </div>
               </div>
               <div className='sm:col-span-6 col-span-8 h-full flex flex-col items-start p-4'>
@@ -77,19 +100,27 @@ const AboutTimeline = () => {
             <div key={i} className='grid grid-cols-12 gap-8 justify-center'>
               <div className=' col-span-5 h-full p-4'></div>
               <div className=' col-span-2 h-full flex flex-col items-center gap-4 p-4'>
-                <div
-                  className='h-24 w-10 rounded-full '
-                  style={{ backgroundColor: "#FCBF02" }}
-                ></div>
+                <div className='h-11 w-11 rounded-full mb-1'>
+                  <img
+                    src={circle}
+                    className='w-full h-full object-cover'
+                    alt=''
+                  />
+                </div>
                 <span className='text-4xl'>{date}</span>
                 <div
-                  className='border-dashed border-r-4 w-1 h-full '
+                  className='w-10 h-full overflow-hidden'
                   style={{ borderColor: "#FCBF02" }}
                 >
-                  {" "}
+                  <img
+                    src={goldenLine}
+                    alt='line'
+                    className='w-full h-full object-fill'
+                    style={{ transform: "scale(6)" }}
+                  />
                 </div>
               </div>
-              <div className=' col-span-5 h-full flex flex-col  p-4'>
+              <div className=' col-span-5 h-full flex flex-col p-4'>
                 <div
                   className='w-7/12 left-point p-4 '
                   style={{ backgroundColor: "#CC0000" }}
@@ -117,16 +148,24 @@ const AboutTimeline = () => {
                 </div>
               </div>
               <div className=' col-span-2 h-full flex flex-col items-center gap-4 p-4'>
-                <div
-                  className='h-24 w-10 rounded-full '
-                  style={{ backgroundColor: "#FCBF02" }}
-                ></div>
+                <div className='h-11 w-11 rounded-full mb-1'>
+                  <img
+                    src={circle}
+                    className='w-full  h-full object-cover'
+                    alt=''
+                  />
+                </div>
                 <span className='text-4xl'>{date}</span>
                 <div
-                  className='border-dashed border-r-4 w-1 h-full '
+                  className='w-10 h-full overflow-hidden'
                   style={{ borderColor: "#FCBF02" }}
                 >
-                  {" "}
+                  <img
+                    src={goldenLine}
+                    alt='line'
+                    className='w-full h-full object-fill'
+                    style={{ transform: item.big ? "scale(5)" : "scale(6)" }}
+                  />
                 </div>
               </div>
               <div className=' col-span-5 h-full p-4'></div>
